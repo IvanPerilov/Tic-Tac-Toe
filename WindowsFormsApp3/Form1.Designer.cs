93f47ef00fd12bf7bc0c572192e1323b8ab1d445
IvanPerilov <lususlatune@gmail.com>
2022-10-12 10:23:09 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.Turn = new System.Windows.Forms.Label();
+            this.turnPlayer = new System.Windows.Forms.Label();
+            this.NGbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 80);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(98, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 80);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(184, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 80);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(12, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 80);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(184, 184);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 80);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(98, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 80);
+            this.button5.TabIndex = 10;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(184, 98);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 80);
+            this.button6.TabIndex = 11;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(12, 184);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 80);
+            this.button7.TabIndex = 12;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(98, 184);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 80);
+            this.button8.TabIndex = 13;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Turn
+            // 
+            this.Turn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Turn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Turn.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Turn.Location = new System.Drawing.Point(312, 12);
+            this.Turn.Name = "Turn";
+            this.Turn.Size = new System.Drawing.Size(361, 112);
+            this.Turn.TabIndex = 14;
+            this.Turn.Text = "Ход игрока";
+            this.Turn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // turnPlayer
+            // 
+            this.turnPlayer.BackColor = System.Drawing.SystemColors.Control;
+            this.turnPlayer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.turnPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.turnPlayer.Location = new System.Drawing.Point(312, 124);
+            this.turnPlayer.Name = "turnPlayer";
+            this.turnPlayer.Size = new System.Drawing.Size(361, 195);
+            this.turnPlayer.TabIndex = 15;
+            this.turnPlayer.Text = "X";
+            this.turnPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NGbutton
+            // 
+            this.NGbutton.Location = new System.Drawing.Point(12, 287);
+            this.NGbutton.Name = "NGbutton";
+            this.NGbutton.Size = new System.Drawing.Size(124, 32);
+            this.NGbutton.TabIndex = 16;
+            this.NGbutton.Text = "Новая игра";
+            this.NGbutton.UseVisualStyleBackColor = true;
+            this.NGbutton.Click += new System.EventHandler(this.NGbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 447);
+            this.Controls.Add(this.NGbutton);
+            this.Controls.Add(this.turnPlayer);
+            this.Controls.Add(this.Turn);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label Turn;
+        private System.Windows.Forms.Label turnPlayer;
+        private System.Windows.Forms.Button NGbutton;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string checkTurn()
+        {
+            if (turn % 2 == 1)
+                return "X";
+            else
+                return "O";
+        }
+        
+        void changTurn()
+        {
+            turnPlayer.Text = checkTurn();
+        }
+        void win()
+        {
+            changMod = false;
+            Turn.Text = "Победил игрок";
+        }
+        void checkingMainDiagonal()
+        {
+            if (button1.Text == button5.Text && button1.Text == button9.Text)
+            {
+                button1.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button9.BackColor = Color.Green;
+                win();
+            }
+        }
+        void checkingSideDiagonal()
+        {
+            if (button3.Text == button5.Text && button3.Text == button7.Text)
+            {
+                button3.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                win();
+            }
+        }
+        void checkingFirstLine()
+        {
+            if (button1.Text == button2.Text && button1.Text == button3.Text)
+            {
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.Green;
+                button3.BackColor = Color.Green;
+                win();
+            }
+        }
+        
+        void checkingSecondLine()
+        {
+            if (button4.Text == button5.Text && button4.Text == button6.Text)
+            {
+                button4.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                win();
+            }
+        }
+        void checkingThirdLine()
+        {
+            if (button7.Text == button8.Text && button7.Text == button9.Text)
+            {
+                button4.BackColor = Color.Green;
+                button5.BackColor = Color.Green;
+                button6.BackColor = Color.Green;
+                win();
+            }
+        }
+        void checkingFirstColumn()
+        {
+            if (button1.Text == button4.Text && button1.Text == button7.Text)
+            {
+                button1.BackColor = Color.Green;
+                button4.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                win();
+            }
+        }
+        void checkingSecondColumn()
+        {
+            if (button2.Text == button5.Text && button2.Text == button8.Text)
+            {
+                button1.BackColor = Color.Green;
+                button4.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                win();
+            }
+        }
+        void checkingThirdColumn()
+        {
+            if (button3.Text == button6.Text && button3.Text == button9.Text)
+            {
+                button1.BackColor = Color.Green;
+                button4.BackColor = Color.Green;
+                button7.BackColor = Color.Green;
+                win();
+            }
+        }
+        
+        bool changMod = true;
+        int turn = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(changMod)
+            if(button1.Text=="")
+            {
+                button1.Text = checkTurn();
+                checkingMainDiagonal();
+                checkingFirstLine();
+                checkingFirstColumn();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (changMod)
+                if (button2.Text == "")
+                {
+                    button2.Text = checkTurn();
+                    checkingFirstLine();
+                    checkingSecondColumn();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (changMod)
+                if (button3.Text == "")
+                {
+                    button3.Text = checkTurn();
+                    checkingFirstLine();
+                    checkingThirdColumn();
+                    checkingSideDiagonal();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+    
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (changMod)
+                if (button4.Text == "")
+                {
+                    button4.Text = checkTurn();
+                    checkingSecondLine();
+                    checkingFirstColumn();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(changMod)
+                if(button5.Text == "")
+                {
+                    button5.Text = checkTurn();
+                    checkingSecondColumn();
+                    checkingSecondLine();
+                    checkingMainDiagonal();
+                    if(changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                    
+                }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(changMod)
+                if(button6.Text == "")
+                {
+                    button6.Text = checkTurn();
+                    checkingSecondLine();
+                    checkingThirdColumn();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if(changMod)
+                if(button7.Text == "")
+                {
+                    button7.Text = checkTurn();
+                    checkingThirdLine();
+                    checkingSideDiagonal();
+                    checkingFirstColumn();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if(changMod)
+                if(button8.Text == "")
+                {
+                    button8.Text = checkTurn();
+                    checkingThirdLine();
+                    checkingSecondColumn();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if(changMod)
+                if(button9.Text == "")
+                {
+                    button9.Text = checkTurn();
+                    checkingThirdLine();
+                    checkingThirdColumn();
+                    checkingMainDiagonal();
+                    if (changMod)
+                    {
+                        turn++;
+                        changTurn();
+                    }
+                }
+        }
+        private void NGbutton_Click(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button1.BackColor = Color.White;
+            button2.Text = "";
+            button2.BackColor = Color.White;
+            button3.Text = "";
+            button3.BackColor = Color.White;
+            button4.Text = "";
+            button4.BackColor = Color.White;
+            button5.Text = "";
+            button5.BackColor = Color.White; 
+            button6.Text = "";
+            button6.BackColor = Color.White;
+            button7.Text = "";
+            button7.BackColor = Color.White;
+            button8.Text = "";
+            button8.BackColor = Color.White;
+            button9.Text = "";
+            button9.BackColor = Color.White;
+            turn = 1;
+            changTurn();
+            changMod = true;
+        }
+    }
+}
